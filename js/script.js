ace29addf2490578aff8d750d4b186916dced762
Gabe Austin <gabriel.austin1@gmail.com>
2016-03-11 07:44:32 -0600

    create named function & get number from user

--- js/script.js
+++ js/script.js
@@ -2,26 +2,19 @@ var numberFromUser = prompt("Input a number");
 var stringToNumber = parseInt(numberFromUser);
 var checkForDecimal = Math.floor(numberFromUser);
 
-function userInput(a) {
+function fizzBuzz(num) {
+  for (var i = 1; i <= num; i++) {
 
-  
-  return a;
-
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+      document.write("Fizz Buzz" + "<br>")
+    } else if (i % 3 == 0) {
+      document.write("Fizz" + "<br>")
+    } else if (i % 5 == 0) {
+      document.write("Buzz" + "<br>")
+    } else {
+      document.write(i + "<br>")
+    };
+  num(numberFromUser);
+  return num;
+  }
 };
-
-for (var i = 1; i <= 100; i++) {
-
-  if ((i % 3 == 0) && (i % 5 == 0)) {
-    document.write("Fizz Buzz" + "<br>")
-  } else if (i % 3 == 0) {
-    document.write("Fizz" + "<br>")
-  } else if (i % 5 == 0) {
-    document.write("Buzz" + "<br>")
-  } else {
-    document.write(i + "<br>")
-  };
-
-}
-
-
-
